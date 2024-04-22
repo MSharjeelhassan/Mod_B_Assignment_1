31fd4189d662255a31901c130f670a1f39e29fbf
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2024-04-22 17:27:23 +0500

    working

---
+++ app.js
@@ -0,0 +1,127 @@
+let a = {
+    mobiles: {
+        Samsung: {
+            SamsungA10: {
+                name: "Samsung A10",
+                ram: "4gb",
+                rom: "64gb",
+                camera: "13",
+                price: "22,000 PKR"
+            },
+            SamsungA20: {
+                name: "Samsung A20",
+                ram: "4gb",
+                rom: "64gb",
+                camera: "18",
+                price: "28,000"
+            },
+            SamsungA30: {
+                name: "Samsung A30",
+                ram: "4gb",
+                rom: "64gb",
+                camera: "25",
+                price: "29,000"
+            }
+        },
+        IPhone: {
+            IPhone11: {
+                name: "IPhone11",
+                ram: "4gb",
+                rom: "64gb",
+                camera: "12",
+                price: "100,000"
+            },
+            IPhone12: {
+                name: "IPhone12",
+                ram: "4gb",
+                rom: "64gb",
+                camera: "12",
+                price: "150,000"
+            },
+            IPhone13: {
+                name: "IPhone13",
+                ram: "6gb",
+                rom: "128gb",
+                camera: "24",
+                price: "200,000"
+            }
+        },
+        Oppo: {
+            OppoV20: {
+                name: "OppoV20",
+                ram: "8gb",
+                rom: "128gb",
+                camera: "64",
+                price: "54,999"
+            },
+            OppoF19: {
+                name: "OppoF19",
+                ram: "6gb",
+                rom: "128gb",
+                camera: "48",
+                price: "36,999"
+            },
+            OppoF11: {
+                name: "OppoF11",
+                ram: "4gb",
+                rom: "64gb",
+                camera: "48",
+                price: "35,999"
+            }
+        },
+        Vivo: {
+            VivoY20: {
+                name: "VivoY20",
+                ram: "4gb",
+                rom: "64gb",
+                camera: "13",
+                price: "26,999"
+            },
+            VivoY21: {
+                name: "VivoY21",
+                ram: "4gb",
+                rom: "64gb",
+                camera: "13",
+                price: "43,999"
+            },
+            VivoY55: {
+                name: "VivoY55",
+                ram: "8gb",
+                rom: "128gb",
+                camera: "50",
+                price: "64,999"
+            }
+        }
+    }
+}
+let brand = document.getElementById('brand');
+
+console.log(brand);
+
+let aMobile = a.mobiles;
+console.log(aMobile);
+for(let name in aMobile)
+{
+    brand.innerHTML += `<option value="${name}">${name}</option>`
+};
+
+let model = document.getElementById('model');
+
+brand.addEventListener('change',()=>{
+    model.setAttribute('class','d-block');
+    model.setAttribute('class','form-select');
+    let modelName = Object.keys(a['mobiles'][brand.value]);
+    console.log(modelName);
+    model.innerHTML = "";
+    for (let name of modelName){
+        model.innerHTML += `<option value="${name}">${name}</option>`;
+    }
+    // console.log(model.innerText)
+    });
+
+    model.addEventListener('change',()=>{
+
+        let modelNameRender = document.getElementById('modelNameRender');
+        modelNameRender.value = model.value;
+        // let modelRender = Object.keys(a["mobiles"][])
+    })
